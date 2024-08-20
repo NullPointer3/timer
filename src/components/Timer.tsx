863cae4dd9226cc7d9733265f44057ca6a8a9588
Noah <nuhumusah874@gmail.com>
2024-08-20 21:57:48 +0000

    styling the app

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -12,26 +12,40 @@ interface Props {
 const Timer = (props: Props) => {
   const elapsedString = renderElapsedString(props.elapsed, props.runningSince)
   return (
-    <div className="">
-      <div>
+    <div 
+      className="grid border-[1px] border-gray-400 rounded-md h-[250px] w-[300px] shadow-sm">
+      <div 
+        className="text-lg text-gray-500 p-4">
         <p>
           {props.title}
         </p>
         <p>
           {props.project}
         </p>
-        <div>
-          <h2>{elapsedString}</h2>
-        </div>
-        <div>
-          <span>
-            <FaEdit/>
-          </span>
-          <span>
-            <FaTrash/>
-          </span>
-        </div>
       </div>
+      <div 
+        className="flex justify-center items-center">
+        <h2 
+          className=" text-3xl font-semibold">
+          {elapsedString}
+        </h2>
+      </div>
+      <div 
+        className="flex justify-end items-end space-x-2 p-4">
+        <span 
+          className="text-gray-400 text-xl ">
+          <FaEdit/>
+        </span>
+        <span 
+          className="text-gray-400 text-xl cursor-pointer">
+          <FaTrash/>
+        </span>
+      </div>
+      <button 
+        type="button" 
+        className="w-full border-[1px] border-green-500 rounded-b-md shadow-sm">
+        Start
+      </button>
     </div>
   )
 }
--- src/components/TimerForm.tsx
+++ src/components/TimerForm.tsx
@@ -8,18 +8,33 @@ interface Props {
 const TimerForm = (props: Props) => {
   const submitText = props.title ? "Update" : "Create"
   return (
-    <div>
-      <div>
-        <div>
-          <label>Title</label>
-          <input placeholder="title" type="text" defaultValue={props.title} />
+    <div className="grid border-[1px] border-gray-400 rounded-md h-[250px] w-[300px] shadow-sm">
+      <div className="grid">
+        <div className="grid pt-2 px-4">
+          <label className="text-[16px]">Title</label>
+          <input
+            className="text-[16px] px-2 py-1 outline-none border-[1px] border-gray-400 rounded-md" 
+            placeholder="title" 
+            type="text" 
+            defaultValue={props.title} 
+          />
         </div>
-        <div>
-          <label>Project</label>
-          <input type="text" placeholder="project" defaultValue={props.project}/>
+        <div className="grid pt-2 px-4"> 
+          <label className="text-[16px]">Project</label>
+          <input
+            className="text-[16px] px-2 py-1 outline-none border-[1px] border-gray-400 rounded-md" 
+            type="text" 
+            placeholder="project" 
+            defaultValue={props.project}/>
         </div>
-        <div>
-          <button type="button">{submitText}</button>
+        <div 
+          className="flex justify-center mt-4 w-full">
+          <button
+            className="w-[1/2] border-[1px] border-blue-400" 
+            type="button"
+          >
+            {submitText}
+          </button>
           <button type="button">Cancel</button>
         </div>
       </div>
